866f0e69f39861505bb11cd578e217fff865891b
sim04ful <sim04ful@gmail.com>
2023-07-13 20:52:15 +0100

    added privacy

---
+++ main/src/pages/privacy.tsx
@@ -0,0 +1,148 @@
+export default () => {
+    return <div className='w-full max-w-6xl m-auto p-20'>
+        <div className='rounded-xl bg-neutral-900 shadow-xl text-neutral-50 p-12 '>
+            <div>
+                <p>Privacy Policy</p>
+                <p>Last Updated: <time>@2023/04/01</time></p>
+                <p>Arible ("us", "we", or "our") operates the www.arible.co website (the "Service"). This page informs you of
+                    our policies regarding the collection, use, and disclosure of personal information when you use our
+                    Service and the choices you have associated with that data.</p>
+                <p>By using the Service, you agree to the collection and use of information in accordance with this policy.</p>
+                <ol type="1">
+                    <li>Information Collection and Use</li>
+                </ol>
+                <p>We collect several different types of information for various purposes to provide and improve our Service to you.</p>
+                <p>(a) Personal Data</p>
+                <p>While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you ("Personal Data"). Personally identifiable information may include, but is not limited to:</p>
+                <ul>
+                    <li>Email address</li>
+                </ul>
+                <ul>
+                    <li>First name and last name</li>
+                </ul>
+                <ul>
+                    <li>Profile pictures</li>
+                </ul>
+                <ul>
+                    <li>Cookies and usage data</li>
+                </ul>
+                <p>(b) Usage Data</p>
+                <p>We may also collect information on how the Service is accessed and used ("Usage Data"). This Usage Data may include information such as your computer's Internet Protocol address (e.g., IP address), browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages, unique device identifiers, and other diagnostic data.</p>
+                <ol type="1">
+                    <li>Tracking &amp; Cookies Data</li>
+                </ol>
+                <p>We use cookies and similar tracking technologies to track the activity on our Service and hold certain information. Cookies are files with a small amount of data, which may include an anonymous unique identifier. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Service.</p>
+                <ol type="1">
+                    <li>Analytics &amp; Third-Party Services</li>
+                </ol>
+                <p>We use third-party Service Providers to monitor and analyze the use of our Service.</p>
+                <p>(a) Google Analytics</p>
+                <p>Google Analytics is a web analytics service offered by Google that tracks and reports website traffic. Google uses the data collected to track and monitor the use of our Service. This data is shared with other Google services. Google may use the collected data to contextualize and personalize the ads of its own advertising network.</p>
+                <p>You can opt-out of having made your activity on the Service available to Google Analytics by installing the Google Analytics opt-out browser add-on. The add-on prevents the Google Analytics JavaScript (ga.js, analytics.js, and dc.js) from sharing information with Google Analytics about visits activity.</p>
+                <p>For more information on the privacy practices of Google, please visit the Google Privacy &amp; Terms web page: <a href="https://policies.google.com/privacy"><strong>https://policies.google.com/privacy</strong></a></p>
+                <p>(b) Clarity</p>
+                <p>Clarity is a website analytics service provided by Microsoft. Clarity helps us understand how users interact with our website by collecting information such as clicks, scrolling, and mouse movements.</p>
+                <p>For more information on the privacy practices of Clarity, please visit the Clarity Privacy Policy web page: <a href="https://privacy.microsoft.com/en-us/privacystatement"><strong>https://privacy.microsoft.com/en-us/privacystatement</strong></a></p>
+                <ol type="1">
+                    <li>Use of Data</li>
+                </ol>
+                <p>Arible uses the collected data for various purposes:</p>
+                <ul>
+                    <li>To provide and maintain our Service</li>
+                </ul>
+                <ul>
+                    <li>To notify you about changes to our Service</li>
+                </ul>
+                <ul>
+                    <li>To allow you to participate in interactive features of our Service when you choose to do so</li>
+                </ul>
+                <ul>
+                    <li>To provide customer support</li>
+                </ul>
+                <ul>
+                    <li>To gather analysis or valuable information so that we can improve our Service</li>
+                </ul>
+                <ul>
+                    <li>To monitor the usage of our Service</li>
+                </ul>
+                <ul>
+                    <li>To detect, prevent, and address technical issues</li>
+                </ul>
+                <figure>
+                    <a href="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%2730%27%20height=%2730%27/%3e"><img src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%2730%27%20height=%2730%27/%3e" /></a>
+                </figure>
+                <p>Events which we offer that are similar to those that you have already purchased or inquired about unless you have opted not to receive such information</p>
+                <ol type="1">
+                    <li>Retention of Data</li>
+                </ol>
+                <p>Arible will retain your Personal Data only for as long as is necessary for the purposes set out in this Privacy Policy. We will retain and use your Personal Data to the extent necessary to comply with our legal obligations (for example, if we are required to retain your data to comply with applicable laws), resolve disputes, and enforce our legal agreements and policies.</p>
+                <p>Arible will also retain Usage Data for internal analysis purposes. Usage Data is generally retained for a shorter period, except when this data is used to strengthen the security or to improve the functionality of our Service, or we are legally obligated to retain this data for longer periods.</p>
+                <ol type="1">
+                    <li>Transfer of Data</li>
+                </ol>
+                <p>Your information, including Personal Data, may be transferred to — and maintained on — computers located outside of your state, province, country, or other governmental jurisdiction where the data protection laws may differ from those of your jurisdiction.</p>
+                <p>If you are located outside the UK and choose to provide information to us, please note that we transfer the data, including Personal Data, globally via CDNs and process it where it may be.</p>
+                <p>Your consent to this Privacy Policy followed by your submission of such information represents your agreement to that transfer.</p>
+                <p>Arible will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place, including the security of your data and other personal information.</p>
+                <ol type="1">
+                    <li>Disclosure of Data</li>
+                </ol>
+                <p>Arible may disclose your Personal Data in the good faith belief that such action is necessary to:</p>
+                <ul>
+                    <li>Comply with a legal obligation</li>
+                </ul>
+                <ul>
+                    <li>Protect and defend the rights or property of Arible</li>
+                </ul>
+                <ul>
+                    <li>Prevent or investigate possible wrongdoing in connection with the Service</li>
+                </ul>
+                <ul>
+                    <li>Protect the personal safety of users of the Service or the public</li>
+                </ul>
+                <ul>
+                    <li>Protect against legal liability</li>
+                </ul>
+                <ol type="1">
+                    <li>Security of Data</li>
+                </ol>
+                <p>The security of your data is important to us, but remember that no method of transmission over the Internet or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Data, we cannot guarantee its absolute security.</p>
+                <ol type="1">
+                    <li>Your Rights</li>
+                </ol>
+                <p>Arible aims to take reasonable steps to allow you to correct, amend, delete, or limit the use of your Personal Data. You have the right to:</p>
+                <ul>
+                    <li>Access the information we hold about you</li>
+                </ul>
+                <ul>
+                    <li>Request the correction of inaccurate or incomplete information</li>
+                </ul>
+                <ul>
+                    <li>Request the deletion of your Personal Data under certain conditions</li>
+                </ul>
+                <ul>
+                    <li>Object to the processing of your Personal Data</li>
+                </ul>
+                <ul>
+                    <li>Request the restriction of the processing of your Personal Data under certain conditions</li>
+                </ul>
+                <ul>
+                    <li>Request the transfer of your Personal Data to another organization or directly to you, under certain conditions</li>
+                </ul>
+                <p>To exercise any of these rights, please contact us using the contact information provided below.</p>
+                <ol type="1">
+                    <li>Changes to This Privacy Policy</li>
+                </ol>
+                <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page. Changes to this Privacy Policy are effective when they are posted on this page.</p>
+                <ol type="1">
+                    <li>Contact Us</li>
+                </ol>
+                <p>If you have any questions about this Privacy Policy, please contact us:</p>
+                <ul>
+                    <li>By email: contact@arible.co</li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+}
